e4885f3a9602693a96622416fb7df68565b7f37b
Mathias Fussenegger <f.mathias@zignar.net>
2021-02-06 11:25:26 +0100

    Add a trigger character based autocompletion extension
    This is quite minimal, all it does is trigger the omnifunc if any of the
    trigger characters is typed in insert mode.
    
    The dap omnifunc implementation is async, so this is non-blocking and
    should be fast.
    
    This should provide a good experience without dap turning into a full
    blown autocompletion plugin.

---
+++ lua/dap/ext/autocompl.lua
@@ -0,0 +1,58 @@
+local M = {}
+local api = vim.api
+local dap = require('dap')
+local timer = nil
+
+
+local function destroy_timer()
+  if timer then
+    timer:stop()
+    timer:close()
+    timer = nil
+  end
+end
+
+
+local function trigger_completion()
+  dap.omnifunc(1, "")
+end
+
+
+function M._InsertCharPre()
+  destroy_timer()
+  local char = api.nvim_get_vvar('char')
+  if tonumber(vim.fn.pumvisible()) == 1 then
+    return
+  end
+  local session = dap.session()
+  if not session then
+    return
+  end
+  local capabilities = session.capabilities or {}
+  local triggers = capabilities.completionTriggerCharacters or {'.'}
+  if vim.tbl_contains(triggers, char) then
+    timer = vim.loop.new_timer()
+    timer:start(150, 0, vim.schedule_wrap(trigger_completion))
+  end
+end
+
+
+function M._InsertLeave()
+  destroy_timer()
+end
+
+
+function M.attach(bufnr)
+  bufnr = bufnr or api.nvim_get_current_buf()
+  vim.cmd(string.format(
+    "autocmd InsertCharPre <buffer=%d> lua require('dap.ext.autocompl')._InsertCharPre()",
+    bufnr
+  ))
+  vim.cmd(string.format(
+    "autocmd InsertLeave <buffer=%d> lua require('dap.ext.autocompl')._InsertLeave()",
+    bufnr
+  ))
+end
+
+
+return M
