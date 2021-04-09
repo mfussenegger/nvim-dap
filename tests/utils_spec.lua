7931692bfad19d065aa93708f0039fad43128966
Mathias Fussenegger <f.mathias@zignar.net>
2021-04-09 20:52:42 +0200

    Add tests to CI
    The basic utils tests that this adds are mostly to get the testing setup
    bootstrapped.

---
+++ tests/utils_spec.lua
@@ -0,0 +1,25 @@
+describe('utils.index_of', function()
+  it('returns index of first item where predicate matches', function()
+    local result = require('dap.utils').index_of(
+      {'a', 'b', 'c'},
+      function(x) return x == 'b' end
+    )
+    assert.are.same(2, result)
+  end)
+end)
+
+describe('utils.to_dict', function()
+  it('converts a list to a dictionary', function()
+    local values = { { k='a', val=1 }, { k='b', val = 2 } }
+    local result = require('dap.utils').to_dict(
+      values,
+      function(x) return x.k end,
+      function(x) return x.val end
+    )
+    local expected = {
+      a = 1,
+      b = 2
+    }
+    assert.are.same(expected, result)
+  end)
+end)
