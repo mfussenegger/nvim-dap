f3e8eca1cb8aa7d85380fdd58c959026bdaa817b
Mathias Fussenegger <f.mathias@zignar.net>
2021-05-05 20:40:24 +0200

    Remove unnecessary `do` nesting from ui module

--- lua/dap/ui.lua
+++ lua/dap/ui.lua
@@ -222,79 +222,78 @@ function M.new_tree(opts)
   return self
 end
 
-do
-  function M.get_last_lnum(bufnr)
-    return api.nvim_buf_call(bufnr, function() return vim.fn.line('$') - 1 end)
-  end
 
-  function M.layer(buf)
-    assert(buf, 'Need a buffer to operate on')
-    local marks = {}
-    local ns = api.nvim_create_namespace('dap.ui_layer_' .. buf)
-    local nshl = api.nvim_create_namespace('dap.ui_layer_hl_' .. buf)
-    return {
-      __marks = marks,
-      --- Render the items and associate each item to the rendered line
-      -- The item and context can then be retrieved using `.get(lnum)`
-      --
-      -- lines between start and end_ are replaced
-      -- If start == end_, new lines are inserted at the given position
-      -- If start == nil, appends to the end of the buffer
-      --
-      -- start is 0-indexed
-      -- end_ is 0-indexed exclusive
-      render = function(xs, render_fn, context, start, end_)
-        start = start or M.get_last_lnum(buf)
-        end_ = end_ or start
-        if end_ > start then
-          local extmarks = api.nvim_buf_get_extmarks(buf, ns, {start, 0}, {end_ - 1, -1}, {})
-          for _, mark in pairs(extmarks) do
-            local mark_id = mark[1]
-            marks[mark_id] = nil
-            api.nvim_buf_del_extmark(buf, ns, mark_id)
-          end
-        end
-        -- This is a dummy call to insert new lines in a region
-        -- the loop below will add the actual values
-        local lines = vim.tbl_map(function() return 'PLACEHOLDER' end, xs)
-        api.nvim_buf_set_lines(buf, start, end_, true, lines)
+function M.get_last_lnum(bufnr)
+  return api.nvim_buf_call(bufnr, function() return vim.fn.line('$') - 1 end)
+end
 
-        for i = start, start + #lines - 1 do
-          local item = xs[i + 1 - start]
-          local text, hl_regions = render_fn(item)
-          text = text:gsub('\n', ' ') -- Might make sense to change this and preserve newlines?
-          api.nvim_buf_set_lines(buf, i, i + 1, true, {text})
-          if hl_regions then
-            for _, hl_region in pairs(hl_regions) do
-              api.nvim_buf_add_highlight(
-                buf, nshl, hl_region[1], i, hl_region[2], hl_region[3])
-            end
-          end
-          local line = api.nvim_buf_get_lines(buf, i, i + 1, true)[1]
-          local mark_id = api.nvim_buf_set_extmark(buf, ns, i, 0, {end_col=(#line - 1)})
-          marks[mark_id] = { mark_id = mark_id, item = item, context = context }
+function M.layer(buf)
+  assert(buf, 'Need a buffer to operate on')
+  local marks = {}
+  local ns = api.nvim_create_namespace('dap.ui_layer_' .. buf)
+  local nshl = api.nvim_create_namespace('dap.ui_layer_hl_' .. buf)
+  return {
+    __marks = marks,
+    --- Render the items and associate each item to the rendered line
+    -- The item and context can then be retrieved using `.get(lnum)`
+    --
+    -- lines between start and end_ are replaced
+    -- If start == end_, new lines are inserted at the given position
+    -- If start == nil, appends to the end of the buffer
+    --
+    -- start is 0-indexed
+    -- end_ is 0-indexed exclusive
+    render = function(xs, render_fn, context, start, end_)
+      start = start or M.get_last_lnum(buf)
+      end_ = end_ or start
+      if end_ > start then
+        local extmarks = api.nvim_buf_get_extmarks(buf, ns, {start, 0}, {end_ - 1, -1}, {})
+        for _, mark in pairs(extmarks) do
+          local mark_id = mark[1]
+          marks[mark_id] = nil
+          api.nvim_buf_del_extmark(buf, ns, mark_id)
         end
-      end,
-
-      --- Get the information associated with a line
-      --
-      -- lnum is 0-indexed
-      get = function(lnum, start_col, end_col)
-        local line = api.nvim_buf_get_lines(buf, lnum, lnum + 1, true)[1]
-        start_col = start_col or 0
-        end_col = end_col or #line
-        local start = {lnum, start_col}
-        local end_ = {lnum, end_col}
-        local extmarks = api.nvim_buf_get_extmarks(buf, ns, start, end_, {})
-        if not extmarks or #extmarks == 0 then
-          return
-        end
-        assert(#extmarks == 1, 'Expecting only a single mark per line and region: ' .. vim.inspect(extmarks))
-        local extmark = extmarks[1]
-        return marks[extmark[1]]
       end
-    }
-  end
+      -- This is a dummy call to insert new lines in a region
+      -- the loop below will add the actual values
+      local lines = vim.tbl_map(function() return 'PLACEHOLDER' end, xs)
+      api.nvim_buf_set_lines(buf, start, end_, true, lines)
+
+      for i = start, start + #lines - 1 do
+        local item = xs[i + 1 - start]
+        local text, hl_regions = render_fn(item)
+        text = text:gsub('\n', ' ') -- Might make sense to change this and preserve newlines?
+        api.nvim_buf_set_lines(buf, i, i + 1, true, {text})
+        if hl_regions then
+          for _, hl_region in pairs(hl_regions) do
+            api.nvim_buf_add_highlight(
+              buf, nshl, hl_region[1], i, hl_region[2], hl_region[3])
+          end
+        end
+        local line = api.nvim_buf_get_lines(buf, i, i + 1, true)[1]
+        local mark_id = api.nvim_buf_set_extmark(buf, ns, i, 0, {end_col=(#line - 1)})
+        marks[mark_id] = { mark_id = mark_id, item = item, context = context }
+      end
+    end,
+
+    --- Get the information associated with a line
+    --
+    -- lnum is 0-indexed
+    get = function(lnum, start_col, end_col)
+      local line = api.nvim_buf_get_lines(buf, lnum, lnum + 1, true)[1]
+      start_col = start_col or 0
+      end_col = end_col or #line
+      local start = {lnum, start_col}
+      local end_ = {lnum, end_col}
+      local extmarks = api.nvim_buf_get_extmarks(buf, ns, start, end_, {})
+      if not extmarks or #extmarks == 0 then
+        return
+      end
+      assert(#extmarks == 1, 'Expecting only a single mark per line and region: ' .. vim.inspect(extmarks))
+      local extmark = extmarks[1]
+      return marks[extmark[1]]
+    end
+  }
 end
 
 
