mfussenegger/nvim-dap

commit e551614be0341df23f0d96b81fb6f002e5ef08fa

Add opts argument to disconnect
Allows to override the `restart` and `terminateDebuggee` arguments

lua/dap/session.lua
@@ -788,11 +788,12 @@ function Session:evaluate(expression, fn)
 end
 
 
-function Session:disconnect()
-  self:request('disconnect', {
+function Session:disconnect(opts)
+  opts = vim.tbl_extend('force', {
     restart = false,
     terminateDebuggee = true;
-  })
+  }, opts)
+  self:request('disconnect', opts)
 end
 
 

lua/dap.lua
@@ -437,10 +437,11 @@ end
 
 
 --- Disconnects an active session
-function M.disconnect()
+function M.disconnect(opts)
   if session then
-    -- Should result in a `terminated` event which closes the session and sets it to nil
-    session:disconnect()
+    session:disconnect(opts)
+    session:close()
+    session = nil
   else
     print('No active session. Doing nothing.')
   end

commit 39a1f10c55e91b0924aca22c2069bdef43eb0531

Fix opts handling in disconnect method
e551614be0341df23f0d96b81fb6f002e5ef08fa broke the `exit` command within
the REPL

lua/dap/session.lua
@@ -792,7 +792,7 @@ function Session:disconnect(opts)
   opts = vim.tbl_extend('force', {
     restart = false,
     terminateDebuggee = true;
-  }, opts)
+  }, opts or {})
   self:request('disconnect', opts)
 end
 