2ef7322f1157ed4c23b62a9c98f42c6557cb7209
dchusovitin <dchusovitin@gmail.com>
2021-06-09 21:06:48 +0300

    Use vim.tbl_count() instead of non_empty

--- lua/dap/session.lua
+++ lua/dap/session.lua
@@ -75,7 +75,7 @@ function Session:run_in_terminal(request)
   end
   local opts = {
     clear_env = false;
-    env = non_empty(body.env) and body.env or vim.empty_dict()
+    env = vim.tbl_count(body.env) > 0 and body.env or vim.empty_dict()
   }
   local jobid = vim.fn.termopen(body.args, opts)
   api.nvim_set_current_win(win)
