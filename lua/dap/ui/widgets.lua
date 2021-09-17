f76b57094c57cea738bab7092229b9a8bdc162f7
Mathias Fussenegger <f.mathias@zignar.net>
2021-09-17 18:25:14 +0200

    Handle invalid windows gracefully in widget api auto-resize

--- lua/dap/ui/widgets.lua
+++ lua/dap/ui/widgets.lua
@@ -76,6 +76,10 @@ end
 
 
 local function resize_window(win, buf)
+  if not api.nvim_win_is_valid(win) then
+    -- Could happen if the user moves the buffer into a new window
+    return
+  end
   local lines = api.nvim_buf_get_lines(buf, 0, -1, true)
   local width = 0
   local height = #lines
