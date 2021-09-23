86a3f092165d1795e6d95ee63c17f1a1437d93a5
danielnehrig <daniel.nehrig@dnehrig.com>
2021-09-23 03:44:44 +0200

    feat(breakpoint): add conditional breakpoint sign define

--- lua/dap.lua
+++ lua/dap.lua
@@ -127,6 +127,7 @@ M.configurations = {}
 
 
 vim.fn.sign_define('DapBreakpoint', {text='B', texthl='', linehl='', numhl=''})
+vim.fn.sign_define("DapBreakpointCondition", { text = "C", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define('DapBreakpointRejected', {text='R', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapLogPoint', {text='L', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapStopped', {text='→', texthl='', linehl='debugPC', numhl=''})
