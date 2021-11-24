792ddf4228886dc9b2c5ad6619337848c7dcdef0
Jacob Pfeifer <jacob@pfeifer.dev>
2021-11-24 00:57:35 -0500

    add endPromptPrefix guard back in

--- lua/dap/repl.lua
+++ lua/dap/repl.lua
@@ -285,6 +285,10 @@ function M.prompt_backspace()
   local currentLine = vim.api.nvim_buf_get_lines(0, currentLineNumber-1, currentLineNumber, false)[1]
   local _, endPromptPrefix = string.find(currentLine, "> ")
 
+  if endPromptPrefix == nil then
+    endPromptPrefix = 0
+  end
+
   if (currentColumnNumber-1) ~= endPromptPrefix then
     vim.api.nvim_buf_set_text(0, currentLineNumber-1, currentColumnNumber-2, currentLineNumber-1, currentColumnNumber-1, {""})
     vim.api.nvim_win_set_cursor(0, {currentLineNumber, currentColumnNumber-2})
