aa8f7cd7b570a698ebe54b59ceea6880a6dd1df2
Mathias Fussenegger <f.mathias@zignar.net>
2022-03-16 21:45:28 +0100

    Remove implicit filetypes inference from load_launchjs
    Closes https://github.com/mfussenegger/nvim-dap/issues/467

--- lua/dap/ext/vscode.lua
+++ lua/dap/ext/vscode.lua
@@ -2,8 +2,8 @@ local dap = require('dap')
 local M = {}
 
 --- Extends dap.configurations with entries read from .vscode/launch.json
---
-function M.load_launchjs(path)
+function M.load_launchjs(path, type_to_filetypes)
+  type_to_filetypes = type_to_filetypes or {}
   local resolved_path = path or (vim.fn.getcwd() .. '/.vscode/launch.json')
   if not vim.loop.fs_stat(resolved_path) then
     return
@@ -21,35 +21,17 @@ function M.load_launchjs(path)
   end
   local data = json_decode(contents)
 
-  -- Groups filetypes of debugee configs by adapters if a config use the adapter.
-  -- { <adapter> = { <filetype> = boolean } }
-  local filetypes_of_adapters = {}
-  for filetype, configs in pairs(dap.configurations) do
-    for _, config in ipairs(configs) do
-      local filetypes = filetypes_of_adapters[config.type] or {}
-      filetypes_of_adapters[config.type] = filetypes
-      filetypes[filetype] = true
-    end
-  end
-
   assert(data.configurations, "launch.json must have a 'configurations' key")
   for _, config in ipairs(data.configurations) do
     assert(config.type, "Configuration in launch.json must have a 'type' key")
-    local filetypes = filetypes_of_adapters[config.type]
-    if filetypes then
-      -- Add this config for filetypes containing configs with this adapter.
-      for filetype, _ in pairs(filetypes) do
-        local configurations = dap.configurations[filetype] or {}
-        dap.configurations[filetype] = configurations
-        table.insert(configurations, config)
-      end
-    else
-      -- Fallback: make an assumption that adapter's name equals to filetype
-      local configurations = dap.configurations[config.type] or {}
-      dap.configurations[config.type] = configurations
+    local filetypes = type_to_filetypes[config.type] or {config.type,}
+    for _, filetype in pairs(filetypes) do
+      local configurations = dap.configurations[filetype] or {}
+      dap.configurations[filetype] = configurations
       table.insert(configurations, config)
     end
   end
 end
+
 
 return M
