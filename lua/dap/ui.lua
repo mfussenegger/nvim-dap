524719387b65ff23676526d5bfaf29448a5f255f
Mathias Fussenegger <f.mathias@zignar.net>
2022-05-31 21:22:17 +0200

    Handle buffer disappearing during async render

--- lua/dap/ui.lua
+++ lua/dap/ui.lua
@@ -192,7 +192,12 @@ function M.new_tree(opts)
     }
     context = vim.tbl_deep_extend('keep', context, extra_context)
     for _, child in pairs(opts.get_children(value)) do
-      layer.render({child}, with_indent(indent, opts.render_child), context, api.nvim_buf_line_count(layer.buf))
+      local ok, line_count = pcall(api.nvim_buf_line_count, layer.buf)
+      if not ok then
+        -- User might have closed the buffer
+        return
+      end
+      layer.render({child}, with_indent(indent, opts.render_child), context, line_count)
       if is_expanded(child) then
         render_all_expanded(layer, child, indent + 2)
       end
