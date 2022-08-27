fc315d5f4994498b9329c6196bd3da9017e6fbb5
Mathias Fussenegger <f.mathias@zignar.net>
2022-08-27 11:40:41 +0200

    Add support for `inputs` in vscode/launch.json

--- tests/ext_vscode_spec.lua
+++ tests/ext_vscode_spec.lua
@@ -8,4 +8,89 @@ describe('dap.ext.vscode', function()
     assert.are.same({ { type = 'bar', request = 'attach', name = "bar test" }, }, dap.configurations.c)
     assert.are.same({ { type = 'bar', request = 'attach', name = "bar test" }, }, dap.configurations.cpp)
   end)
+
+  it('supports promptString input', function()
+    local prompt
+    local default
+    vim.fn.input = function(prompt_, default_)
+      prompt = prompt_
+      default = default_
+      return 'Fake input'
+    end
+    local vscode = require('dap.ext.vscode')
+    local jsonstr = [[
+      {
+        "configurations": [
+          {
+            "type": "dummy",
+            "request": "launch",
+            "name": "Dummy",
+            "program": "${workspaceFolder}/${input:myInput}"
+          }
+        ],
+        "inputs": [
+          {
+            "id": "myInput",
+            "type": "promptString",
+            "description": "Your input",
+            "default": "the default value"
+          }
+        ]
+      }
+    ]]
+    local configs = vscode._load_json(jsonstr)
+    local ok = false
+    local result
+    coroutine.wrap(function()
+      result = configs[1].program()
+      ok = true
+    end)()
+    vim.wait(1000, function() return ok end)
+    assert.are.same("${workspaceFolder}/Fake input", result)
+    assert.are.same("Your input: ", prompt)
+    assert.are.same("the default value", default)
+  end)
+
+  it('supports pickString input', function()
+    local options
+    local opts
+    local vscode = require('dap.ext.vscode')
+    vim.ui.select = function(options_, opts_, on_choice)
+      options = options_
+      opts = opts_
+      on_choice(options_[1])
+    end
+    local jsonstr = [[
+      {
+        "configurations": [
+          {
+            "type": "dummy",
+            "request": "launch",
+            "name": "Dummy",
+            "program": "${workspaceFolder}/${input:myInput}"
+          }
+        ],
+        "inputs": [
+          {
+            "id": "myInput",
+            "type": "pickString",
+            "options": ["one", "two", "three"],
+            "description": "Select input"
+          }
+        ]
+      }
+    ]]
+    local configs = vscode._load_json(jsonstr)
+    local ok = false
+    local result
+    coroutine.wrap(function()
+      result = configs[1].program()
+      ok = true
+    end)()
+    vim.wait(1000, function() return ok end)
+    assert.are.same(true, ok, "coroutine must finish")
+    assert.are.same("${workspaceFolder}/one", result)
+    assert.are.same("Select input", opts.prompt)
+    assert.are.same({"one", "two", "three"}, options)
+  end)
 end)
