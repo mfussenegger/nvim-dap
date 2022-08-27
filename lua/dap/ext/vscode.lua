cc44f047f329528f053f7dd0154ad1ba1891c516
Mathias Fussenegger <f.mathias@zignar.net>
2022-08-27 11:40:41 +0200

    Add support for `inputs` in vscode/launch.json

--- lua/dap/ext/vscode.lua
+++ lua/dap/ext/vscode.lua
@@ -1,5 +1,104 @@
 local dap = require('dap')
+local notify = require('dap.utils').notify
 local M = {}
+
+
+local function create_input(type_, input)
+  if type_ == "promptString" then
+    return function()
+      local description = input.description or 'Input'
+      if not vim.endswith(description, ': ') then
+        description = description .. ': '
+      end
+      return vim.fn.input(description, input.default)
+    end
+  elseif type_ == "pickString" then
+    return function()
+      local options = assert(input.options, "input of type pickString must have an `options` property")
+      local opts = {
+        prompt = input.description
+      }
+      local co = coroutine.running()
+      vim.ui.select(options, opts, function(option)
+        vim.schedule(function()
+          coroutine.resume(co, option or input.default)
+        end)
+      end)
+      return coroutine.yield()
+    end
+  else
+    local msg = "Unsupported input type in vscode launch.json: " .. type_
+    notify(msg, vim.log.levels.WARN)
+  end
+end
+
+
+local function create_inputs(inputs)
+  local result = {}
+  for _, input in ipairs(inputs) do
+    local id = assert(input.id, "input must have a `id`")
+    local key = "${input:" .. id .. "}"
+    local type_ = assert(input.type, "input must have a `type`")
+    local fn = create_input(type_, input)
+    if fn then
+      result[key] = fn
+    end
+  end
+  return result
+end
+
+
+local function apply_input(inputs, value)
+  if type(value) ~= "string" then
+    return value
+  end
+  local input_id = string.match(value, "${input:(%w+)}")
+  if not input_id then
+    return value
+  end
+  local input_key = "${input:" .. input_id .. "}"
+  local input = inputs[input_key]
+  if input then
+    return function()
+      assert(coroutine.running(), "Must run in coroutine")
+      local input_value = input()
+      local result = value:gsub(input_key, input_value)
+      return result
+    end
+  else
+    local msg = "No input with id `" .. input_id .. "` found in inputs"
+    notify(msg, vim.log.levels.WARN)
+    return value
+  end
+end
+
+
+local function apply_inputs(config, inputs)
+  local result = {}
+  for key, value in pairs(config) do
+    result[key] = apply_input(inputs, value)
+  end
+  return result
+end
+
+
+function M._load_json(jsonstr)
+  local decode = vim.json and vim.json.decode or vim.fn.json_decode
+  local data = decode(jsonstr)
+  local inputs = create_inputs(data.inputs or {})
+  local has_inputs = next(inputs) ~= nil
+
+  if has_inputs and data.configurations then
+    local configs = {}
+    for _, config in ipairs(data.configurations) do
+      table.insert(configs, apply_inputs(config, inputs))
+    end
+    return configs
+  end
+
+  return data.configurations
+end
+
 
 --- Extends dap.configurations with entries read from .vscode/launch.json
 function M.load_launchjs(path, type_to_filetypes)
@@ -15,14 +114,10 @@ function M.load_launchjs(path, type_to_filetypes)
     end
   end
   local contents = table.concat(lines, '\n')
-  local json_decode = vim.fn.json_decode
-  if vim.json then
-    json_decode = vim.json.decode
-  end
-  local data = json_decode(contents)
+  local configurations = M._load_json(contents)
 
-  assert(data.configurations, "launch.json must have a 'configurations' key")
-  for _, config in ipairs(data.configurations) do
+  assert(configurations, "launch.json must have a 'configurations' key")
+  for _, config in ipairs(configurations) do
     assert(config.type, "Configuration in launch.json must have a 'type' key")
     assert(config.name, "Configuration in launch.json must have a 'name' key")
     local filetypes = type_to_filetypes[config.type] or { config.type, }
