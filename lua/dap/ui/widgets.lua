284c754f60f6ef15ecdd071524b945cb2fffbbb9
Mathias Fussenegger <f.mathias@zignar.net>
2022-12-19 18:54:04 +0100

    Stop resizing widgets if converted to non-float window

--- lua/dap/ui/widgets.lua
+++ lua/dap/ui/widgets.lua
@@ -114,7 +114,9 @@ local function resizing_layer(win, buf)
   local orig_render = layer.render
   layer.render = function(...)
     orig_render(...)
-    resize_window(win, buf)
+    if api.nvim_win_get_config(win).relative ~= '' then
+      resize_window(win, buf)
+    end
   end
   return layer
 end
