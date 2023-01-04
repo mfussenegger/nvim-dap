0f482cad01e31b67cb4e41e6cb66b346d5742408
Mathias Fussenegger <f.mathias@zignar.net>
2023-01-04 14:38:09 +0100

    Make dap.utils import in dap.lua lazy

--- lua/dap.lua
+++ lua/dap.lua
@@ -1,5 +1,4 @@
 local api = vim.api
-local utils = require('dap.utils')
 local M = {}
 
 ---@type Session|nil
@@ -16,6 +15,10 @@ local lazy = setmetatable({}, {
 
 local function log()
   return require('dap.log').create_logger('dap.log')
+end
+
+local function notify(...)
+  lazy.utils.notify(...)
 end
 
 
@@ -193,7 +196,7 @@ local function eval_option(option)
 end
 
 local var_placeholders_once = {
-  ['${command:pickProcess}'] = utils.pick_process
+  ['${command:pickProcess}'] = lazy.utils.pick_process
 }
 
 local var_placeholders = {
@@ -270,7 +273,7 @@ local function run_adapter(adapter, configuration, opts)
     lazy.progress.report('Running: ' .. name)
     M.attach(adapter, configuration, opts)
   else
-    utils.notify(string.format('Invalid adapter type %s, expected `executable` or `server`', adapter.type), vim.log.levels.ERROR)
+    notify(string.format('Invalid adapter type %s, expected `executable` or `server`', adapter.type), vim.log.levels.ERROR)
   end
 end
 
@@ -307,7 +310,7 @@ local function select_config_and_run()
     )
   )
   if #configurations == 0 then
-    utils.notify(string.format('No configuration found for `%s`. You need to add configs to `dap.configurations.%s` (See `:h dap-configuration`)', filetype, filetype), vim.log.levels.INFO)
+    notify(string.format('No configuration found for `%s`. You need to add configs to `dap.configurations.%s` (See `:h dap-configuration`)', filetype, filetype), vim.log.levels.INFO)
     return
   end
   lazy.ui.pick_if_many(
@@ -318,7 +321,7 @@ local function select_config_and_run()
       if configuration then
         M.run(configuration)
       else
-        utils.notify('No configuration selected', vim.log.levels.INFO)
+        notify('No configuration selected', vim.log.levels.INFO)
       end
     end
   )
@@ -366,13 +369,13 @@ function M.run(config, opts)
         config
       )
     elseif adapter == nil then
-      utils.notify(string.format(
+      notify(string.format(
         'The selected configuration references adapter `%s`, but dap.adapters.%s is undefined',
         config.type,
         config.type
       ), vim.log.levels.ERROR)
     else
-      utils.notify(string.format(
+      notify(string.format(
           'Invalid adapter `%s` for config `%s`. Expected a table or function. '
             .. 'Read :help dap-adapter and define a valid adapter.',
           vim.inspect(adapter),
@@ -391,7 +394,7 @@ function M.run_last()
   if last_run then
     M.run(last_run.config, last_run.opts)
   else
-    utils.notify('No configuration available to re-run', vim.log.levels.INFO)
+    notify('No configuration available to re-run', vim.log.levels.INFO)
   end
 end
 
@@ -407,7 +410,7 @@ function M.restart_frame()
   if session then
     session:restart_frame()
   else
-    utils.notify('No active session', vim.log.levels.INFO)
+    notify('No active session', vim.log.levels.INFO)
   end
 end
 
@@ -424,8 +427,8 @@ function M.step_into(opts)
 
   session:request('stepInTargets', { frameId = session.current_frame.id }, function(err, response)
     if err then
-      utils.notify(
-        'Error on step_into: ' .. utils.fmt_error(err) .. ' (while requesting stepInTargets)',
+      notify(
+        'Error on step_into: ' .. lazy.utils.fmt_error(err) .. ' (while requesting stepInTargets)',
         vim.log.levels.ERROR
       )
       return
@@ -437,7 +440,7 @@ function M.step_into(opts)
       function(target) return target.label end,
       function(target)
         if not target or not target.id then
-          utils.notify('No target selected. No stepping.', vim.log.levels.INFO)
+          notify('No target selected. No stepping.', vim.log.levels.INFO)
         else
           opts.targetId = target.id
           session:_step('stepIn', opts)
@@ -457,7 +460,7 @@ function M.step_back(opts)
   if session.capabilities.supportsStepBack then
     session:_step('stepBack', opts)
   else
-    utils.notify('Debug Adapter does not support stepping backwards.', vim.log.levels.ERROR)
+    notify('Debug Adapter does not support stepping backwards.', vim.log.levels.ERROR)
   end
 end
 
@@ -466,7 +469,7 @@ function M.reverse_continue(opts)
   if session.capabilities.supportsStepBack then
     session:_step('reverseContinue', opts)
   else
-    utils.notify('Debug Adapter does not support stepping backwards.', vim.log.levels.ERROR)
+    notify('Debug Adapter does not support stepping backwards.', vim.log.levels.ERROR)
   end
 end
 
@@ -479,7 +482,7 @@ end
 
 
 function M.stop()
-  utils.notify('dap.stop() is deprecated. Call dap.close() instead', vim.log.levels.WARN)
+  notify('dap.stop() is deprecated. Call dap.close() instead', vim.log.levels.WARN)
   M.close()
 end
 
@@ -550,13 +553,13 @@ function M.restart()
   if session.capabilities.supportsRestartRequest then
     session:request('restart', nil, function(err0, _)
       if err0 then
-        utils.notify('Error restarting debug adapter: ' .. utils.fmt_error(err0), vim.log.levels.ERROR)
+        notify('Error restarting debug adapter: ' .. lazy.utils.fmt_error(err0), vim.log.levels.ERROR)
       else
-        utils.notify('Restarted debug adapter', vim.log.levels.INFO)
+        notify('Restarted debug adapter', vim.log.levels.INFO)
       end
     end)
   else
-    utils.notify('Restart not supported', vim.log.levels.ERROR)
+    notify('Restart not supported', vim.log.levels.ERROR)
   end
 end
 
@@ -575,7 +578,7 @@ function M.list_breakpoints(open_quickfix)
   })
   if open_quickfix then
     if #qf_list == 0 then
-      utils.notify('No breakpoints set!', vim.log.levels.INFO)
+      notify('No breakpoints set!', vim.log.levels.INFO)
     else
       api.nvim_command('copen')
     end
@@ -625,18 +628,18 @@ function M.set_exception_breakpoints(filters, exceptionOptions)
   if session then
     session:set_exception_breakpoints(filters, exceptionOptions)
   else
-    utils.notify('Cannot set exception breakpoints: No active session!', vim.log.levels.INFO)
+    notify('Cannot set exception breakpoints: No active session!', vim.log.levels.INFO)
   end
 end
 
 
 function M.run_to_cursor()
   if not session then
-    utils.notify('Cannot use run_to_cursor without active session', vim.log.levels.INFO)
+    notify('Cannot use run_to_cursor without active session', vim.log.levels.INFO)
     return
   end
   if not session.stopped_thread_id then
-    utils.notify('run_to_cursor can only be used if stopped at a breakpoint', vim.log.levels.INFO)
+    notify('run_to_cursor can only be used if stopped at a breakpoint', vim.log.levels.INFO)
     return
   end
 
@@ -760,7 +763,7 @@ function M.disconnect(opts, cb)
   if session then
     session:disconnect(opts, cb)
   else
-    utils.notify('No active session. Doing nothing.', vim.log.levels.INFO)
+    notify('No active session. Doing nothing.', vim.log.levels.INFO)
     if cb then
       cb()
     end
@@ -781,7 +784,7 @@ end
 ---@param bwc_dummy any
 function M.attach(adapter, config, opts, bwc_dummy)
   if type(adapter) == 'string' then
-    utils.notify(
+    notify(
       'dap.launch signature changed from (host, port, config) to (adapter, config), please adjust',
       vim.log.levels.WARN
     )
@@ -792,14 +795,14 @@ function M.attach(adapter, config, opts, bwc_dummy)
     adapter = { type = 'server', host = host, port = port, }
   end
   if not config.request then
-    utils.notify('Config needs the `request` property which must be one of `attach` or `launch`', vim.log.levels.ERROR)
+    notify('Config needs the `request` property which must be one of `attach` or `launch`', vim.log.levels.ERROR)
     return
   end
   assert(adapter.port, 'Adapter used with attach must have a port property')
   session = require('dap.session'):connect(adapter, opts, function(err)
     if err then
       vim.schedule(function()
-        utils.notify(
+        notify(
           string.format("Couldn't connect to %s:%s: %s", adapter.host or '127.0.0.1', adapter.port, err),
           vim.log.levels.ERROR
         )
