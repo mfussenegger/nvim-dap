0d472b6ee76fd032347f3dd2897ad1e37b751b8c
Mathias Fussenegger <f.mathias@zignar.net>
2025-04-19 21:36:38 +0200

    fixup! error message for launch.json parsing

--- lua/dap.lua
+++ lua/dap.lua
@@ -312,10 +312,11 @@ end
 providers.configs["dap.launch.json"] = function()
   local ok, configs = pcall(require("dap.ext.vscode").getconfigs)
   if not ok then
-    local msg = string.format("Cannot get configurations from `launch.json`:\n%s", configs)
-    notify(msg, vim.log.levels.ERROR)
+    local msg = "Can't get configurations from launch.json:\n%s" .. configs
+    vim.notify_once(msg, vim.log.levels.WARN, {title = "DAP"})
+    return {}
   end
-  return ok and configs or {}
+  return configs
 end
 
 do
